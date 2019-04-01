09f86958b61a441c053e63ec648c49a32b9c0833
sourav143 <49021287+sourav143@users.noreply.github.com>
2019-04-01 15:33:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const VLCPlayerControl = {
   VLCPlayer: require('./VLCPlayer').default,
-  VlCPlayerView: require('./playerView/index').default,
+//   VlCPlayerView: require('./playerView/index').default,
   VlCPlayerViewByMethod: require('./playerViewByMethod/index').default,
   VlcSimplePlayer: require('./playerViewByMethod/index').default,
 };
